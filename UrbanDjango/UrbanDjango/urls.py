233d1e7e37cf237d01ae1431265c517187220c35
AlinaHan1 <alinakoshenko@gmail.com>
2025-02-07 01:10:33 +0300

    Add files via upload

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import Class_view, func_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('class/', Class_view.as_view()),
+    path('func/', func_view),
 ]
